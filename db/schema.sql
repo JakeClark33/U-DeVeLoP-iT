1a0562ca2a1bd4ac608569bd9526f1e885d81c59
JakeClark33 <jakemclark@hotmail.com>
2021-08-22 15:00:13 -0500

    parties created

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE parties (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    description TEST
+    description TEXT
 );
 
-CREATE TABLE  candidates (
+CREATE TABLE candidates (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
